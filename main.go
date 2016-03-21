7f4c1fffe4825161df995cc8098c39fe769334a7
Shay Tsadok <shayts7@gmail.com>
2016-03-21 17:00:25 +0200

    add logrus as pse logger

--- main.go
+++ main.go
@@ -1,36 +1,52 @@
 /*
 	TODO:
-	- circleCI
 	- send / receive
 	- Json processing
-	- log
 	- http server
 	- tests
- */
+*/
 package main
 
 import (
-	"os/signal"
+	"github.com/Sirupsen/logrus"
 	"os"
+	"os/signal"
 	"syscall"
-	"fmt"
 )
+
+var log = logrus.New()
+
+func init() {
+
+	//set log level according to env var
+	logLevel := os.Getenv("PSE_LOG_LEVEL")
+	if logLevel != "" {
+		level, err := logrus.ParseLevel(logLevel)
+		if err != nil {
+			log.Error("Failed to set logger level: ", err)
+		} else {
+			log.Level = level
+		}
+	} else {
+		log.Level = logrus.DebugLevel
+	}
+}
 
 func main() {
 
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
+	sigs := make(chan os.Signal)
+	done := make(chan bool)
 
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		sig := <-sigs
-		fmt.Println()
-		fmt.Println("Signal received:", sig)
+
+		log.Warn("Signal received: ", sig)
 		done <- true
 	}()
 
-	fmt.Println("awaiting signal")
+	log.Info("awaiting signal")
 	<-done
-	fmt.Println("exiting")
+	log.Warn("exiting")
 }
