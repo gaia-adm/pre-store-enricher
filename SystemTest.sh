4fa7a818458a9ade8d824c44bbfa4df4e0b976f0
Shay Tsadok <shayts7@gmail.com>
2016-04-03 12:53:05 +0000

    working on system test

--- SystemTest.sh
+++ SystemTest.sh
@@ -1,15 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 #running rabbit and pse
-docker run -d -p 5673:5672 -p 15673:15672 -e RABBITMQ_USER="myuser" -e RABBITMQ_PASS="mypass" --name rabbitmq tutum/rabbitmq
+docker run -d -p 5672:5672 -p 15672:15672 -e RABBITMQ_USER="myuser" -e RABBITMQ_PASS="mypass" --name rabbitmq tutum/rabbitmq
 docker run -d -e PSE_AMQP_URL="amqp://myuser:mypass@rabbitmq:5672" --link rabbitmq:rabbitmq --name pse gaiaadm/pre-store-enricher
 
 #creating dummy queue (test-pse-q) and bind it to event-to-index exchange
-curl -i -u myuser:mypass -H "content-type:application/json" -XPUT -d'{"vhost":"/","name":"test-pse-q","durable":"false","auto_delete":"false","arguments":{}}' http://localhost:15672/api/queues/%2F/test-pse-q
-curl -i -u myuser:mypass -H "content-type:application/json"   -XPOST -d'{"vhost":"/","source":"events-to-index","destination_type":"q","destination":"test-pse-q","routing_key":"events.#","arguments":{}}' http://localhost:15672/api/bindings/%2F/e/events-to-index/q/test-pse-q
+curl --fail -S -i -u myuser:mypass -H "content-type:application/json" -XPUT -d'{"vhost":"/","name":"test-pse-q","durable":"true","auto_delete":"false","arguments":{}}' http://localhost:15672/api/queues/%2F/test-pse-q
+curl --fail -S -i -u myuser:mypass -H "content-type:application/json" -XPOST -d'{"vhost":"/","source":"events-to-index","destination_type":"q","destination":"test-pse-q","routing_key":"event.#","arguments":{}}' http://localhost:15672/api/bindings/%2F/e/events-to-index/q/test-pse-q
 
 #send message to events-to-enrich exchange
-curl -i -u myuser:mypass -H "content-type:application/json" -XPOST -d'{"vhost":"/","name":"events-to-enrich","properties":{"ery_mode":"2","payload":"{\"key\": \"value\"}","headers":{},"props":{},"payload_encoding":"string"}' http://localhost:15672/api/exchanges/%2f/events-to-enrich/publish
+curl --fail -S -i -u myuser:mypass -H "content-type:application/json" -XPOST -d'{"vhost":"/","name":"events-to-enrich","properties":{"delivery_mode":2,"headers":{}},"routing_key":"event.testme","delivery_mode":"2","payload":"{\"key\": \"value\"}","headers":{},"props":{},"payload_encoding":"string"}' http://localhost:15672/api/exchanges/%2f/events-to-enrich/publish
 
 #test that the message reached the dummy queue with the enrichment of gaia section
-curl -i -u myuser:mypass -H "content-type:application/json"   -XPOST -d'{"vhost":"/","name":"test-pse-q","truncate":"50000","requeue":"true","encoding":"auto","count":"1"}' http://localhost:15672/api/queues/%2F/test-pse-q/get | grep gaia | grep incoming_time
+curl --fail -S -i -u myuser:mypass -H "content-type:application/json" -XPOST -d'{"vhost":"/","name":"test-pse-q","truncate":"50000","requeue":"true","encoding":"auto","count":"1"}' http://localhost:15672/api/queues/%2F/test-pse-q/get | grep gaia | grep incoming_time2
